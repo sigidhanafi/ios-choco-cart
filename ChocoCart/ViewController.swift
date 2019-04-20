96dc7ddfffab75ffcf09e285db3541938c25b42c
sigidhanafi <sigidhanafi@gmail.com>
2019-04-21 00:12:13 +0700

    Initial Commit

---
+++ ChocoCart/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChocoCart
+//
+//  Created by Sigit Hanafi on 21/04/19.
+//  Copyright © 2019 Sigit Hanafi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
